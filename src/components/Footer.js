5240ef484532d30df3351bdcbb655fd6a0e12e3f
mafiageek <anton_kho@msn.com>
2023-01-30 19:27:12 +0800

    added initial footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        bottom: 0,
+        position: "fixed",
+      }}
+    >
+      Footer
+    </Box>
+  );
+};
+
+export default Footer;
