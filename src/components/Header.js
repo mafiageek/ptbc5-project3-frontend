196df1065b2eec13e4ad461a0dc3082fb6baa642
mafiageek <anton_kho@msn.com>
2023-01-30 19:25:32 +0800

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,39 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Box, TextField } from "@mui/material";
-import { Login, ShoppingCart } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Stack,
+  IconButton,
+  Button,
+} from "@mui/material";
+import { CatchingPokemonOutlined } from "@mui/icons-material";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <AppBar sx={{ backgroundColor: "#282C34" }}>
       <Toolbar>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            flexGrow: 1,
-            flexDirection: { xs: "column", sm: "row" },
-            mt: 2,
-            mb: 2,
-          }}
+        <IconButton>
+          <CatchingPokemonOutlined sx={{ color: "white" }} />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component={Link}
+          to={"/"}
+          sx={{ flexGrow: 1, textDecoration: "none", color: "white" }}
         >
-          <Typography
-            component={Link}
-            to={"/"}
-            variant="h6"
-            sx={{ mr: "auto", textDecoration: "none", color: "white" }}
-          >
-            xxx
-          </Typography>
-          <TextField
-            size="small"
-            sx={{
-              backgroundColor: "white",
-              border: 0,
-              mr: 2,
-            }}
-            id="search"
-            variant="outlined"
-          />
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <ShoppingCart />
-            <Typography
-              variant="body1"
-              sx={{ mr: 2, ml: 1, textDecoration: "none", color: "white" }}
-            >
-              cart
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <Login />
-            <Typography
-              variant="body1"
-              sx={{ mr: 2, ml: 1, textDecoration: "none", color: "white" }}
-            >
-              login
-            </Typography>
-          </Box>
-        </Box>
+          Home
+        </Typography>
+        <Stack direction="row" space={2}>
+          <Button color="inherit" onClick={() => navigate("/products")}>
+            Shop
+          </Button>
+          <Button color="inherit">Cart</Button>
+          <Button color="inherit">Login</Button>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
