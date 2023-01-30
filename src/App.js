7a8ed0313f4ba0ec7c83809c53a31b2115d4221c
mafiageek <anton_kho@msn.com>
2023-01-30 19:28:31 +0800

    updated product route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ProductsPage, ProductPage } from "./pages/";
-import { Header } from "./components";
-
+import ProductPage from "./pages/ProductPage";
+import ProductsPage from "./pages/ProductsPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<ProductPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
