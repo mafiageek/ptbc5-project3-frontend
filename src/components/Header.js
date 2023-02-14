1ae2684963d2aa4a37008b154aa762fa3285ba4c
mafiageek <anton_kho@msn.com>
2023-02-14 18:45:14 +0800

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useCart } from "../context/cart";
 import {
   AppBar,
@@ -8,14 +8,53 @@ import {
   IconButton,
   Button,
   Badge,
+  Avatar,
 } from "@mui/material";
 import { CatchingPokemonOutlined } from "@mui/icons-material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { ShoppingCartOutlined } from "@mui/icons-material";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 const Header = () => {
+  const location = useLocation();
   const navigate = useNavigate();
   const [cart] = useCart();
+  const { isAuthenticated, loginWithRedirect, user, logout } = useAuth0();
+
+  const handleLogin = async () => {
+    loginWithRedirect()
+      .then(() => {
+        console.log("locationState", location.state);
+        navigate(location.state);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`http://localhost:3001/users?email=${user.email}`)
+        .then(({ data }) => {
+          if (data.length === 0) {
+            axios.post(`http://localhost:3001/users`, {
+              name: user.name,
+              role: "user",
+              email: user.email,
+              uid: user.sub,
+            });
+          }
+        });
+    }
+  }, [user]);
+
+  console.log(user);
+  const handleLogout = async () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
   return (
     <AppBar sx={{ backgroundColor: "#282C34" }}>
       <Toolbar>
@@ -39,7 +78,19 @@ const Header = () => {
               <ShoppingCartOutlined />
             </Badge>
           </Button>
-          <Button color="inherit">Login</Button>
+          {!isAuthenticated && (
+            <Button color="inherit" onClick={handleLogin}>
+              Login
+            </Button>
+          )}
+          {isAuthenticated && (
+            <>
+              <Button color="inherit" onClick={handleLogout}>
+                Logout
+              </Button>
+              <Avatar src={user.picture} />
+            </>
+          )}
         </Stack>
       </Toolbar>
     </AppBar>
