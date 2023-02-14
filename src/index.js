666aab5c3ec222fb4d3da274e4737537aca64009
mafiageek <anton_kho@msn.com>
2023-02-14 18:43:54 +0800

    added auth0

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { CartProvider } from "./context/cart";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <Auth0Provider
+      domain="dev-zldrlxetiu43era6.us.auth0.com"
+      clientId="5Xv0y6Epm2Md9C8q1Zbs0SNlQ3Ef3VqM"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
