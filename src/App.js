c18e1fedabd90a64943ff528f2862a4ce4a6a316
mafiageek <anton_kho@msn.com>
2023-02-14 18:42:50 +0800

    added route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import ProductPage from "./pages/ProductPage";
 import ProductsPage from "./pages/ProductsPage";
 import CartPage from "./pages/CartPage";
@@ -7,17 +8,26 @@ import AdminPage from "./pages/AdminPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AddProductPage from "./pages/AddProductPage";
+import HomePage from "./pages/HomePage";
+import UpdateProductPage from "./pages/UpdateProductPage";
+import OrderPage from "./pages/OrderPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Toaster position="top-center" />
       <Routes>
+        <Route path="" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductPage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/order" element={<OrderPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/addproduct" element={<AddProductPage />} />
+        <Route path="/updateproduct/:id" element={<UpdateProductPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
