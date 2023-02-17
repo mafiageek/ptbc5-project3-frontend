51a115c79c2c76846ff12a3972e15dbb0fc8e8d5
mafiageek <anton_kho@msn.com>
2023-02-17 21:26:01 +0800

    updated order page

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useCart } from "../context/cart";
+import { useNavigate } from "react-router-dom";
 import {
   Container,
   Paper,
@@ -11,29 +12,69 @@ import {
   Table,
   TableBody,
   TableRow,
-  TextField,
+  List,
+  ListItem,
 } from "@mui/material";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const OrderPage = () => {
-  const { user, isAuthenticated } = useAuth0();
-  const [addresses, setAddresses] = useState([]);
-  const [cart] = useCart();
+  const { user } = useAuth0();
+  const [address, setAddress] = useState({
+    userId: "",
+    id: "",
+    address: "",
+    city: "",
+    postal: "",
+    country: "",
+  });
+  const [cart, setCart] = useCart();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (isAuthenticated) {
-      axios
-        .get(`http://localhost:3001/users?email=${user?.email}`)
-        .then(({ data }) => {
-          return axios.get(`http://localhost:3001/users/${data[0].id}`);
-        })
-        .then(({ data }) => {
-          console.log("addresses", data.userAddresses);
-          setAddresses(data.userAddresses);
+    axios
+      .get(`http://localhost:3001/addresses?email=${user?.email}`)
+      .then(({ data }) => {
+        setAddress({
+          userId: data[0].userId,
+          id: data[0].id,
+          address: data[0].addressLine1,
+          city: data[0].city,
+          postal: data[0].postal,
+          country: data[0].country,
         });
-    }
-  }, [isAuthenticated, user.email]);
+      });
+  }, [user?.email]);
+
+  const handleOrder = () => {
+    axios
+      .post(`http://localhost:3001/orders`, {
+        userId: address.userId,
+        userAddressId: address.id,
+        total: total,
+        orderStatus: "paid",
+      })
+      .then(({ data }) => {
+        cart.map((item) =>
+          axios.post(`http://localhost:3001/orderItems`, {
+            orderId: data.id,
+            productId: item.id,
+            quantity: item.stock,
+          })
+        );
+      })
+      .then(() => {
+        toast.success("Thank you for your purchase");
+        localStorage.removeItem("cart");
+        setCart("");
+        navigate("/products");
+      });
+  };
+
+  const total = cart?.reduce((accumulator, object) => {
+    return accumulator + Number(object.price);
+  }, 0);
 
   return (
     <Container maxWidth="md" sx={{ mt: 10 }}>
@@ -44,11 +85,45 @@ const OrderPage = () => {
               <TableBody>
                 <TableRow>
                   <TableCell>
-                    <Typography variant="h6">SHIPPING</Typography>
-                    <Stack direction="column" spacing={1}>
+                    {/* <Stack direction="column" spacing={1}>
                       {addresses?.map((a) => (
                         <TextField value={a.addressLine1} />
                       ))}
+                    </Stack> */}
+                    <Typography variant="h6">SHIPPING</Typography>
+                    <Stack direction="row" spacing={1} sx={{ p: 1 }}>
+                      <Stack component={Paper} sx={{ minWidth: 250 }}>
+                        <List>
+                          <ListItem>{address.address}</ListItem>
+                          <ListItem>{address.city}</ListItem>
+                          <ListItem>{address.postal}</ListItem>
+                          <ListItem>{address.country}</ListItem>
+                        </List>
+                      </Stack>
+                      <Stack direction="column">
+                        {address ? (
+                          <Button
+                            onClick={() =>
+                              navigate(`/updateaddress/${address.id}`, {
+                                state: "/order",
+                              })
+                            }
+                            size="small"
+                            variant="contained"
+                            sx={{ backgroundColor: "#282C34", m: 1 }}
+                          >
+                            Edit
+                          </Button>
+                        ) : (
+                          <Button
+                            size="small"
+                            variant="contained"
+                            sx={{ backgroundColor: "#282C34", m: 1 }}
+                          >
+                            Update
+                          </Button>
+                        )}
+                      </Stack>
                     </Stack>
                   </TableCell>
                 </TableRow>
@@ -57,15 +132,11 @@ const OrderPage = () => {
                     <Typography variant="h6">PAYMENT</Typography>
                   </TableCell>
                 </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Typography variant="h6">ORDER ITEMS</Typography>
-                  </TableCell>
-                </TableRow>
               </TableBody>
             </Table>
           </TableContainer>
         </Stack>
+        <Stack></Stack>
         <Stack sx={{ mt: 2, pl: 2 }}>
           <TableContainer component={Paper}>
             <Table sx={{ width: 200 }}>
@@ -86,6 +157,7 @@ const OrderPage = () => {
                 <TableRow>
                   <TableCell align="center">
                     <Button
+                      onClick={handleOrder}
                       size="small"
                       variant="contained"
                       sx={{ backgroundColor: "#282C34" }}
@@ -98,6 +170,33 @@ const OrderPage = () => {
             </Table>
           </TableContainer>
         </Stack>
+      </Stack>
+      <Stack>
+        <TableContainer>
+          <Table sx={{ maxWidth: 600 }}>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="h6">ORDER ITEMS</Typography>
+                </TableCell>
+              </TableRow>
+              {cart.map((product) => (
+                <TableRow key={product.key}>
+                  <TableCell>
+                    <img
+                      width="50"
+                      src={product.productImages[0]?.urlString}
+                      alt="product"
+                    />
+                  </TableCell>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>{product.price}</TableCell>
+                  <TableCell>{product.stock}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Stack>
     </Container>
   );
