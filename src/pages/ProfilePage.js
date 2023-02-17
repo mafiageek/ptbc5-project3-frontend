9e40a9e8cf442001e11bd2db456ce5c0a2f8bb95
mafiageek <anton_kho@msn.com>
2023-02-17 21:25:00 +0800

    added order history

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,97 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Paper,
   Typography,
-  Button,
-  Stack,
   TableCell,
   TableContainer,
   Table,
   TableBody,
   TableRow,
-  TextField,
   TableHead,
 } from "@mui/material";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const ProfilePage = () => {
+  const [orders, setOrders] = useState([]);
+
+  const { user } = useAuth0();
+
+  useEffect(() => {
+    const getOrderItems = async () => {
+      const { data } = await axios.get(
+        `http://localhost:3001/users?email=${user.email}`
+      );
+
+      setOrders(data[0].orders);
+    };
+
+    getOrderItems();
+  }, [user?.email]);
+
   return (
     <Container sx={{ mt: 10 }} maxWidth="md">
-      <Stack direction="row">
-        <Stack direction="column" spacing={1}>
-          <Typography variant="h6">PROFILE</Typography>
-          <TextField variant="outlined" label="Name" name="name" size="small" />
-          <TextField
-            variant="outlined"
-            label="Email"
-            name="email"
-            size="small"
-          />
-          <TextField
-            variant="outlined"
-            label="Address"
-            name="address"
-            size="small"
-          />
-          <TextField variant="outlined" label="City" name="city" size="small" />
-          <TextField
-            variant="outlined"
-            label="Country"
-            name="country"
-            size="small"
-          />
-          <TextField
-            variant="outlined"
-            label="Postal Code"
-            name="postalCode"
-            size="small"
-          />
-          <Button
-            size="small"
-            variant="contained"
-            sx={{ backgroundColor: "#282C34", m: 1 }}
-          >
-            UPDATE
-          </Button>
-        </Stack>
-
-        <Stack sx={{ pl: 4 }}>
-          <Typography variant="h6">MY ORDERS</Typography>
-          <TableContainer component={Paper} sx={{ mt: 1 }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>DATE</TableCell>
-                  <TableCell>TOTAL</TableCell>
-                  <TableCell>PAID</TableCell>
-                  <TableCell>DELIVERED</TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>ACTION</TableCell>
-                  <TableCell>ACTION</TableCell>
-                  <TableCell>ACTION</TableCell>
-                  <TableCell>ACTION</TableCell>
-                  <TableCell>ACTION</TableCell>
-                  <TableCell>
-                    {" "}
-                    <Button
-                      size="small"
-                      variant="contained"
-                      sx={{ backgroundColor: "#282C34", m: 1 }}
-                    >
-                      DETAIL
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Stack>
-      </Stack>
+      <Typography variant="h6">MY ORDERS</Typography>
+      <TableContainer component={Paper} sx={{ mt: 1 }}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>DATE</TableCell>
+              <TableCell>TOTAL</TableCell>
+              <TableCell>STATUS</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{order.id}</TableCell>
+                <TableCell>{order.createdAt.substring(0, 10)}</TableCell>
+                <TableCell>${order.total}</TableCell>
+                <TableCell>{order.orderStatus}</TableCell>
+                {/* <TableCell>
+                  <Button
+                    size="small"
+                    variant="contained"
+                    sx={{ backgroundColor: "#282C34", m: 1 }}
+                  >
+                    DETAIL
+                  </Button>
+                </TableCell> */}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
