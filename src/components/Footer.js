0b1369e8c57dfcc5837734ac0cec4e6bbfc2627d
mafiageek <anton_kho@msn.com>
2023-02-17 21:22:46 +0800

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
     <Box
+      component="footer"
       sx={{
         display: "flex",
         flexDirection: "column",
         bottom: 0,
         position: "fixed",
+        backgroundColor: "#282C34",
+        height: 40,
+        width: "100%",
+        justifyContent: "center",
       }}
     >
-      Footer
+      <Typography variant="body2" align="center" sx={{ color: "whitesmoke" }}>
+        kedai bodega &copy; 2023
+      </Typography>
     </Box>
   );
 };
